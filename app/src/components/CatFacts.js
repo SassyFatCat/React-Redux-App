ce380478b1b34f3004f1e6d70b0ecc60d3290c54
Ryan Lee <therealsassyfatcat@outlook.com>
2020-08-12 14:55:06 -0500

    saving

--- app/src/components/CatFacts.js
+++ app/src/components/CatFacts.js
@@ -1,6 +1,18 @@
 import React, { useEffect } from 'react';
 import {connect} from 'react-redux';
 import {fetchFacts} from '../store';
+import styled from 'styled-components';
+
+const Fact = styled.p`
+margin: 2% auto;
+padding: 1%;
+width: 50%;
+background-color: lightpink;
+color: white;
+border: 2px solid lightblue;
+border-radius: 5px;
+`;
+
 
 const CatFacts = props => {
 useEffect(() => {
@@ -10,7 +22,7 @@ useEffect(() => {
 return (
     <div>
         {props.facts.map(fact => {
-            return <p>{fact.text}</p>
+            return <Fact>{fact.text}</Fact>
         })}
     </div>
 )
